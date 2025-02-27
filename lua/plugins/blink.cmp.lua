3bd13ed0681dee5a2ee26af87ab25df8f45c180c
dijith-481 <dijithdinesh@protonmail.com>
2025-02-27 15:48:41 +0530

    blink modification

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -1,17 +1,158 @@
-return{
-  'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = 'rafamadriz/friendly-snippets',
-  version = '*',
-  opts = {
-   keymap = { preset = 'default' },
+return {
 
-    appearance = {
-     use_nvim_cmp_as_default = true,
-     nerd_font_variant = 'mono'
-    },
+	"saghen/blink.cmp",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"mikavilpas/blink-ripgrep.nvim",
+		"folke/lazydev.nvim",
+		"folke/snacks.nvim",
+		"onsails/lspkind.nvim",
+		"milanglacier/minuet-ai.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
 
-    },
+	version = "*",
+	enabled = function()
+		return not vim.tbl_contains({ "markdown" }, vim.bo.filetype)
+			and vim.bo.buftype ~= "prompt"
+			and vim.b.completion ~= false
+	end,
+
+	opts = {
+		completion = {
+			menu = {
+				draw = {
+					components = {
+						kind_icon = {
+							ellipsis = false,
+							text = function(ctx)
+								local lspkind = require("lspkind")
+								local icon = ctx.kind_icon
+								if vim.tbl_contains({ "Path" }, ctx.source_name) then
+									local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+									if dev_icon then
+										icon = dev_icon
+									end
+								else
+									icon = lspkind.symbolic(ctx.kind, {
+										mode = "symbol",
+									})
+								end
+
+								return icon .. ctx.icon_gap
+							end,
+
+							highlight = function(ctx)
+								local hl = ctx.kind_hl
+								if vim.tbl_contains({ "Path" }, ctx.source_name) then
+									local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+									if dev_icon then
+										hl = dev_hl
+									end
+								end
+								return hl
+							end,
+						},
+					},
+				},
+			},
+		},
+
+		signature = { enabled = true },
+		keymap = {
+			preset = "default",
+			["<A-1>"] = {
+				function(cmp)
+					cmp.accept({ index = 1 })
+				end,
+			},
+			["<A-2>"] = {
+				function(cmp)
+					cmp.accept({ index = 2 })
+				end,
+			},
+			["<A-3>"] = {
+				function(cmp)
+					cmp.accept({ index = 3 })
+				end,
+			},
+			["<A-4>"] = {
+				function(cmp)
+					cmp.accept({ index = 4 })
+				end,
+			},
+			["<A-5>"] = {
+				function(cmp)
+					cmp.accept({ index = 5 })
+				end,
+			},
+			["<A-6>"] = {
+				function(cmp)
+					cmp.accept({ index = 6 })
+				end,
+			},
+			["<A-7>"] = {
+				function(cmp)
+					cmp.accept({ index = 7 })
+				end,
+			},
+			["<A-8>"] = {
+				function(cmp)
+					cmp.accept({ index = 8 })
+				end,
+			},
+			["<A-9>"] = {
+				function(cmp)
+					cmp.accept({ index = 9 })
+				end,
+			},
+			["<A-0>"] = {
+				function(cmp)
+					cmp.accept({ index = 10 })
+				end,
+			},
+			["<A-y>"] = require("minuet").make_blink_map(),
+		},
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "mono",
+		},
+		-- },
+		sources = {
+			default = {
+				"lsp",
+				"lazydev",
+				"buffer",
+				"snippets",
+				"path",
+				"ripgrep",
+				"minuet",
+			},
+
+			providers = {
+				path = {
+					opts = {
+						get_cwd = function(_)
+							return vim.fn.getcwd()
+						end,
+					},
+				},
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					score_offset = 100,
+				},
+				ripgrep = {
+					module = "blink-ripgrep",
+					name = "Ripgrep",
+					score_offset = 4,
+				},
+				minuet = {
+					name = "minuet",
+					module = "minuet.blink",
+					score_offset = 8,
+				},
+			},
+		},
+	},
 }
-
-
