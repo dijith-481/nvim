5bb23f2c487a569dee52fa60050eb4f722542bc6
dijith-481 <dijithdinesh@protonmail.com>
2025-03-01 23:46:08 +0530

    removed some plugins

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -20,38 +20,18 @@ return {
 			})
 		end,
 	},
-	{ "nvim-tree/nvim-web-devicons", opts = {} },
 	{
-		"numToStr/Comment.nvim",
-		event = { "BufReadPre", "BufNewFile" },
 		"refractalize/oil-git-status.nvim",
 		dependencies = {
 			"stevearc/oil.nvim",
 		},
-		config = function()
-			-- import comment plugin safely
-			local comment = require("Comment")
-
-			local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-			-- enable comment
-			comment.setup({
-				-- for commenting tsx, jsx, svelte, html files
-				pre_hook = ts_context_commentstring.create_pre_hook(),
-			})
-		end,
+		config = true,
+		event = { "VeryLazy" },
 	},
 	{
 		"HiPhish/rainbow-delimiters.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		lazy = true,
 		config = function() end,
-	},
-
-	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
-
-	{
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
 	},
 }
