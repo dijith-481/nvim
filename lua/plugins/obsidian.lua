0f2324fd2870fe0f85943b87d7c1da4885646e97
dijith-481 <dijithdinesh@protonmail.com>
2025-03-01 21:21:38 +0530

    obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,45 +1,38 @@
 return {
 	{
 		"epwalsh/obsidian.nvim",
-		version = "*", -- recommended, use latest release instead of latest commit
-		lazy = true,
-		ft = "markdown",
-		-- },
+		version = "*",
+		lazy = false,
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
-		opts = {
-			workspaces = {
-				{
-					name = "personal",
-					path = "~/syncthing/obsidian/",
-				},
-			},
-		},
 		config = function()
+			require("obsidian").setup({
+				ui = { enable = false },
+				workspaces = {
+					{
+						name = "personal",
+						path = "~/syncthing/obsidian/",
+					},
+				},
+			})
 			vim.opt.conceallevel = 2
 		end,
-	},
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			-- "echasnovski/mini.icons",
-			"nvim-tree/nvim-web-devicons",
-		}, -- if you prefer nvim-web-devicons
-		---@module 'render-markdown'
-		---@type render.md.UserConfig
-		opts = {},
 	},
 	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		build = "cd app && npm install",
-
-		-- Set the global variable to the function name
 		init = function()
 			vim.g.mkdp_filetypes = { "markdown" }
 		end,
 		ft = { "markdown" },
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		ft = { "markdown" },
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
 	},
 }
