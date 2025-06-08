2854105cf1c8e64d746aabe5bfcff482bc560d2a
dijith-481 <dijithdinesh@protonmail.com>
2025-06-08 17:46:01 +0530

    fix lsp not loading

--- lsp/ts_ls.lua
+++
@@ -1,9 +0,0 @@
-return {
-	cmd = { "typescript-language-server", "--stdio" },
-	-- capabilities = { capabilities },
-	settings = {
-		format = { enable = true },
-		-- disable unused vars hint
-		-- diagnostics = { ignoredCodes = { 6133, 2304 } },
-	},
-}
