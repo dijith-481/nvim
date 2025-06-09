9272a142a8506b75c3c9b5300a9e53d42f4ed0af
dijith-481 <dijithdinesh@protonmail.com>
2025-06-09 23:45:18 +0530

    update options

--- lua/dij/options.lua
+++ lua/dij/options.lua
@@ -1,72 +1,46 @@
--- vim.o.cmdheight = 0
--- vim.o.cursorcolumn = true
-vim.opt.laststatus = 3
 vim.g.have_nerd_font = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = "a"
-vim.opt.showmode = false
-vim.opt.clipboard = "unnamedplus"
 vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.smartindent = true
--- vim.opt.wrap = true -- TODO
-vim.o.winborder = "rounded"
--- Keep signcolumn on by default
-vim.opt.signcolumn = "number"
--- vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
-vim.opt.hlsearch = true
-vim.opt.numberwidth = 3
-
--- Decrease update time
-vim.opt.updatetime = 250
--- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
--- Preview substitutions live, as you type!
-vim.opt.inccommand = "split"
-vim.opt.tabstop = 2
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-
--- Show which line your cursor is on
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cmdheight = 1
+vim.opt.cmdwinheight = 8
+vim.opt.copyindent = true              -- TODO check
 vim.opt.cursorline = true
-
--- turn off swapfile
-vim.opt.swapfile = false
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 6
-
---termgui colors
-vim.o.termguicolors = true
-
-vim.opt.formatoptions:remove({ "c", "r", "o" })
--- vim.opt.iskeyword:append("-")
-vim.opt.iskeyword:append("_")
-vim.opt.iskeyword:append("-")
-vim.opt.whichwrap:append("<,>,[,]")
+vim.opt.debug = ""                     --  default value
 vim.opt.diffopt:append("linematch:60") -- higher probability of  line match in diff
-
--- 	local cur_num
--- 	if vim.v.virtnum ~= 0 then
--- 		return "|"
--- 	end
--- 	cur_num = vim.v.lnum
--- 	return "%=%=%l%=" .. get_fold(cur_num) .. " "
--- end
--- vim.o.foldcolumn = "0"
-
--- vim.o.statuscolumn = "%!v:lua.get_statuscol()"
-
+vim.opt.expandtab = true
+vim.opt.foldclose = ""                 --  "|all"
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
--- vim.o.foldenable = true
--- vim.o.lazyredraw = true
-
--- vim.o.statuscolumn = require("dij.statuscolumn")
+vim.opt.formatoptions:remove({ "c", "r", "o" })
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.inccommand = "split"
+vim.opt.incsearch = true
+vim.opt.laststatus = 3
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.mps:append("<:>")
+vim.opt.mouse = "a"
+vim.opt.mousescroll = "ver:2,hor:1"
+vim.opt.number = true
+vim.opt.numberwidth = 3
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 5
+vim.opt.shiftwidth = 2
+vim.opt.showmode = false
+vim.opt.showtabline = 1
+vim.opt.signcolumn = "number" -- TODO
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.smoothscroll = true -- partially implemented
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.swapfile = false
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 300
+vim.opt.undofile = true
+vim.opt.updatetime = 250
+vim.opt.whichwrap:append("<,>,[,]")
+vim.opt.winblend = 30
+vim.opt.winborder = "rounded"
